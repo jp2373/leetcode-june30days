2679c65b54066d7d856725e91a674a95669ba9c1
Jeel Pujara <jeelpujara2373@gmail.com>
2020-06-11 18:39:40 +0530

    Create day9.py

---
+++ solutions/day9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        s_i, t_i = 0, 0
+        
+        while s_i < len(s) and t_i < len(t):
+            s_i, t_i = s_i + (s[s_i] == t[t_i]), t_i + 1
+            
+        return s_i == len(s)
